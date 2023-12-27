599c582916fd206c79e09e1b793b6ac53e9fe581
lmpascual <leilamaypascual@gmail.com>
2023-12-26 22:49:16 -0500

    moved normalizing spects before cutting windows

--- src/getSpectMatrix.m
+++ src/getSpectMatrix.m
@@ -1,4 +1,4 @@
-function [spects_matrix,labels_all] = getSpectMatrix(syllables,params)
+function [spects_matrix,labels_all,S] = getSpectMatrix(syllables,params)
 % Import the necessary libraries
 import matlab.io.*
 import statistics.toolbox.*
@@ -93,14 +93,15 @@ for i = 1:length(recmat)
             syl_wav = rawsong(on_id:off_id); %raw waveform for current syllable
             [S1] = spect_from_waveform(syl_wav,Fs,0,spect_params); %get syllable spectrogram
             S{x,i} = S1;
-            % cut the spectrograms to equal duration lengths
-            S_cut = S1(:,end-numWindows:end);
-            % reshape multidim spec to 1 row for each trial            
-            S_collapsed = reshape(S_cut,1,[]);
-            Slog = log(1+abs(S_collapsed)); % use log-scale for visualization
+            Slog = log(1+abs(S1)); % use log-scale for visualization
             
+            % reshape multidim spec to 1 row for each trial            
+            S_collapsed = reshape(Slog,1,[]);
+            
+            % cut the spectrograms to equal duration lengths
+            S_cut = S_collapsed(:,end-numWindows:end);
             % concatenate all spect trials
-            spects_matrix = [spects_matrix; Slog]; % use log-scale for visualization
+            spects_matrix = [spects_matrix; S_cut]; % use log-scale for visualization
         end
     end
 end
