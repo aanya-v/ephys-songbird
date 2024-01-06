7152da5ab5eb8158c5236c20b0ee6f68b960d4da
lmpascual <leilamaypascual@gmail.com>
2024-01-06 09:49:38 -0500

    adds the output of the raw audio files from the getSpectMatrix.m function

--- src/compileSylSpects.m
+++ src/compileSylSpects.m
@@ -6,8 +6,13 @@ function [spects_all,labels_all,days_all] = compileSylSpects(birdname,syls,days,
 spects_all = [];
 labels_all = [];
 days_all = [];
+labels_by_day = {};
 spect_uncut = {};
+raw_audio = {};
 failedSyls ={};
+
+% print spect parameters
+spect_params
 
 % Concatenate spectrograms, syllable labels, and days
 for d= 1:length(days)
@@ -18,13 +23,16 @@ for d= 1:length(days)
 
     spect_day = [];
     labels_day = [];
+    spect_uncut_day=[];
+    raw_audio_day = {};
     try
-    [spect_day, labels_day,spect_uncut_day] = getSpectMatrix(syls, spect_params);
+    [spect_day, labels_day,spect_uncut_day, raw_audio_day] = getSpectMatrix(syls, spect_params);
     [spects_all] = [spects_all; spect_day];
     [labels_all] = [labels_all, labels_day];
     [days_all] = [days_all; repmat(day,size(spect_day,1),1)];
     spect_uncut{d} = spect_uncut_day;
     labels_by_day{d} = labels_day;
+    raw_audio = [raw_audio;raw_audio_day];
 
     catch exception
         message = ['failed for: ' num2str(day) ' -syl ' syls];
@@ -40,6 +48,9 @@ end
 days_all(days_all == 661 | days_all == 662) = 66;
 days(days == 661 | days == 662) = 66;
 
+% %% Create a spectrogram matrix of all trials padding with zero trials that are less than the maximum syllable duration
+% spects_all_zero_padded = spectsZeroPad(spect_uncut);
+
 %% Save output to processed data folder
 cd(savedir)
 
@@ -48,8 +59,10 @@ daysString = strjoin(arrayfun(@(x) num2str(x), days, 'UniformOutput', false), '_
 dateGenerated = datetime();
 
 % Concatenate the parts to create the filename and save
-filename = strcat('spectrograms_syls_', syls, '_days', daysString, '_windows_', spect_params.whichWindows, '.mat');
+whichWinStr = sprintf('_syldur_%d_to_%d', spect_params.whichWindows(1), spect_params.whichWindows(2));
+filename = strcat('spectrograms_',birdname,'_syls_', syls, '_days_', daysString, whichWinStr, '.mat');
 save(filename,"birdname","syls","days","spects_all","labels_by_day",...
-    "labels_all","days_all","spect_uncut", "spect_params",...
-    "dateGenerated")
+    "labels_all","days_all","spect_uncut", "raw_audio", "spect_params",...
+    "dateGenerated","failedSyls",'-v7.3' )
+% save(filename,"spects_all_zero_padded", '-append');
 end
